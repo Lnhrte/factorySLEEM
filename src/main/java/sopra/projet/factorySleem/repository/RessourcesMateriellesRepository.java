141eae8aff33ff8d149ede9fae736c698a3e26b8
mira <mira.toubassy@gmail.com>
2019-02-05 14:41:48 +0100

    ressourcesHumaines repo

---
+++ src/main/java/sopra/projet/factorySleem/repository/RessourcesMateriellesRepository.java
@@ -0,0 +1,25 @@
+package sopra.projet.factorySleem.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import sopra.projet.factorySleem.model.Ordinateur;
+import sopra.projet.factorySleem.model.RessourcesMaterielles;
+import sopra.projet.factorySleem.model.Salle;
+import sopra.projet.factorySleem.model.VideoProjecteur;
+
+public interface RessourcesMateriellesRepository extends JpaRepository<RessourcesMaterielles, String>{
+	
+//	@Query("select ordinateur from RessourcesMaterielles")
+//	List<Ordinateur>findAllOrdinateur();
+	
+	List<RessourcesMaterielles>findAll();
+	List<Ordinateur>findAllOrdinateur();
+	List<VideoProjecteur>findAllVideoProjecteur();
+	List<Salle>findAllSalle();
+	
+	
+	
+	
+}
