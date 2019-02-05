3756dafcca0f564bbc378b19bfb7644f3e9174c9
Elodie <elodie.linhart@free.fr>
2019-02-05 10:04:00 +0100

    ressourcesHumaines
    constructor without id

--- src/test/java/sopra/projet/factrorySleem/SpringBoot/FormationSpringBootApplicationTestsSekouly.java
+++ src/test/java/sopra/projet/factrorySleem/SpringBoot/FormationSpringBootApplicationTestsSekouly.java
@@ -1,4 +1,4 @@
-package sopra.formation.formationSpringBoot;
+package sopra.projet.factrorySleem.SpringBoot;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class FormationSpringBootApplicationTests {
+public class FormationSpringBootApplicationTestsSekouly {
 
 	@Test
 	public void contextLoads() {
