e4417cf0176b8eb6ef9cd51e554882e14f4a09fa
Etienne <tinou_78700@hotmail.fr>
2019-02-06 16:35:45 +0100

    Securité fonctionnelle

--- src/main/java/sopra/projet/factorySleem/configuration/SecurityConfiguration.java
+++ src/main/java/sopra/projet/factorySleem/configuration/SecurityConfiguration.java
@@ -21,8 +21,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	@Autowired
 	private DataSource dataSource;
 
-//	@Autowired
-//	private CustomUserDetailService userDetailsService;
+	@Autowired
+	private CustomUserDetailService userDetailsService;
 
 	// definir les pages securisees
 	@Override
@@ -30,20 +30,25 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 		// http.csrf().disable().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
 		// .authorizeRequests().antMatchers(HttpMethod.OPTIONS).anonymous();
 		// http.authorizeRequests().antMatchers("/rest/**").permitAll();
+		// http.headers().frameOptions().disable();
+
+		http.csrf().disable();
 		http.headers().frameOptions().disable();
 
-		http.authorizeRequests().antMatchers("/**").authenticated().and().formLogin().loginPage("/login")
+		http.authorizeRequests().antMatchers("/").permitAll();
+		http.authorizeRequests().antMatchers("/matiere/").authenticated().and().formLogin().loginPage("/login")
 				.failureUrl("/login?error=erreur").permitAll().and().logout().permitAll()
-				.logoutSuccessUrl("/matiere/");
+				.logoutSuccessUrl("/");
 		//
-		http.authorizeRequests().antMatchers("/rest/**").authenticated().and().httpBasic();
+		// http.authorizeRequests().antMatchers("/rest/**").authenticated().and().httpBasic();
 	}
 
 	// // definition la methode d'authentification
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication().withUser("olivier").password("{noop}olivier").roles("Administrateur");
-	}
+//	 @Override
+//	 protected void configure(AuthenticationManagerBuilder auth) throws Exception
+//	 {
+//	 auth.inMemoryAuthentication().withUser("olivier").password("{noop}olivier").roles("Administrateur");
+//	 }
 
 	// @Override
 	// protected void configure(AuthenticationManagerBuilder auth) throws
@@ -55,11 +60,10 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	// username=?");
 	// }
 
-	// @Override
-	// protected void configure(AuthenticationManagerBuilder auth) throws Exception
-	// {
-	// auth.userDetailsService(userDetailsService).passwordEncoder(getPasswordEncoder());
-	// }
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userDetailsService).passwordEncoder(getPasswordEncoder());
+	}
 
 	@Bean(name = "passwordEncoder")
 	public PasswordEncoder getPasswordEncoder() {
--- src/main/java/sopra/projet/factorySleem/services/CustomUserDetailService.java
+++ src/main/java/sopra/projet/factorySleem/services/CustomUserDetailService.java
@@ -1,33 +1,31 @@
- package sopra.projet.factorySleem.services;
+package sopra.projet.factorySleem.services;
 
- import java.util.Optional;
+import java.util.Optional;
 
- import org.springframework.beans.factory.annotation.Autowired;
- import org.springframework.security.core.userdetails.UserDetails;
- import org.springframework.security.core.userdetails.UserDetailsService;
- import
- org.springframework.security.core.userdetails.UsernameNotFoundException;
- import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 import sopra.projet.factorySleem.model.Utilisateur;
 import sopra.projet.factorySleem.repository.UtilisateurRepository;
 
- @Service
- public class CustomUserDetailService implements UserDetailsService {
+@Service
+public class CustomUserDetailService implements UserDetailsService {
 
- @Autowired
- private UtilisateurRepository utilisateurRepository;
+	@Autowired
+	private UtilisateurRepository utilisateurRepository;
 
- @Override
- public UserDetails loadUserByUsername(String arg0) throws
- UsernameNotFoundException {
- Optional<Utilisateur> opt = utilisateurRepository.findByIdWithRoles(arg0);
+	@Override
+	public UserDetails loadUserByUsername(String arg0) throws UsernameNotFoundException {
+		Optional<Utilisateur> opt = utilisateurRepository.findByIdWithRoles(arg0);
 
- if (opt.isPresent()) {
- return new CustomUserDetails(opt.get());
- } else {
- throw new UsernameNotFoundException("utilisateur inconnu");
- }
- }
+		if (opt.isPresent()) {
+			return new CustomUserDetails(opt.get());
+		} else {
+			throw new UsernameNotFoundException("utilisateur inconnu");
+		}
+	}
 
- }
+}
