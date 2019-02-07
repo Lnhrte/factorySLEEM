c48dad039cbe77f7c0ca97e89c3303d30c524087
Etienne <tinou_78700@hotmail.fr>
2019-02-07 15:51:29 +0100

    modulerepo 2

--- src/main/java/sopra/projet/factorySleem/repository/ModuleRepository.java
+++ src/main/java/sopra/projet/factorySleem/repository/ModuleRepository.java
@@ -1,9 +1,15 @@
 package sopra.projet.factorySleem.repository;
 
+import java.util.ArrayList;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import sopra.projet.factorySleem.model.Module;
 
 public interface ModuleRepository extends JpaRepository<Module, Long> {
 
+	@Query("select m from Module m where m.formation.id=:id")
+	ArrayList<Module> findByIdWithFormation(@Param("id") Long id);
 }
